2bfd38d024e8ceda5f245ee01aaeccc2d69876c0
tmpstpdwn <tmpstpdwn@proton.me>
2025-08-31 14:08:37 +0530

    renamed the project

---
+++ HashVault.java
@@ -0,0 +1,6 @@
+public class HashVault
+{
+    public static void main(string args[]){
+        System.out.println("Hello world from HashVault!!!");
+    }
+}
--- Passman.java
+++
@@ -1,7 +0,0 @@
-public class PasswordManager
-{
-    public static void main(string args[]){
-        System.out.println("Hello world");
-        System.out.print("hELLO");
-    }
-}
